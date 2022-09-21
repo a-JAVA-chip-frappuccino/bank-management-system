b8e290d3746a746492446b67fb6622d96fe0e35f
Eleanor Kelman <64874489+a-JAVA-chip-frappuccino@users.noreply.github.com>
2022-09-21 13:34:14 -0400

    testing server-client connection

---
+++ src/main/java/com/springframework/bank/controller/BankController.java
@@ -0,0 +1,15 @@
+package com.springframework.bank.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BankController {
+    
+    @RequestMapping("/")
+    public String sendToScreen(Model model) {
+        return "bank/account";
+    }
+
+}
